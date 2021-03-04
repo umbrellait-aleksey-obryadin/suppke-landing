407ea6023659d37b5677928b79101f3186df082b
Aleksey Obryadin <alekseyobryadin@alekseyobryadin.localdomain>
2021-03-04 10:22:10 +0300

    dev: section and scripts, refactoring code

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+// настрока слайдеров
 $(".order__carousel_type_top").slick({
   slidesToShow: 1,
   slidesToScroll: 1,
@@ -6,27 +7,126 @@ $(".order__carousel_type_top").slick({
   asNavFor: ".order__carousel_type_bottom",
 });
 $(".order__carousel_type_bottom").slick({
-  slidesToShow: 4,
+  slidesToShow: 3,
   slidesToScroll: 1,
   asNavFor: ".order__carousel_type_top",
   dots: false,
   focusOnSelect: true,
   arrows: false,
+  mobileFirst: true,
+  infinite: true,
+  responsive: [
+    {
+      breakpoint: 1200,
+      settings: {
+        slidesToShow: 4,
+        slidesToScroll: 1,
+      },
+    },
+    {
+      breakpoint: 992,
+      settings: {
+        slidesToShow: 4,
+        slidesToScroll: 1,
+      },
+    },
+    {
+      breakpoint: 767,
+      settings: {
+        slidesToShow: 4,
+        slidesToScroll: 1,
+      },
+    },
+    {
+      breakpoint: 575,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 1,
+      },
+    },
+  ],
 });
 
 $(".brends__carousel").slick({
-  slidesToShow: 5,
-  slidesToScroll: 1,
   dots: false,
   infinite: true,
-  focusOnSelect: true,
+  speed: 1000,
   arrows: false,
+  slidesToShow: 5,
+  slidesToScroll: 2,
+  arrows: false,
+
+  responsive: [
+    {
+      breakpoint: 1200,
+      settings: {
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        infinite: true,
+      },
+    },
+    {
+      breakpoint: 992,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 1,
+      },
+    },
+    {
+      breakpoint: 767,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        arrows: false,
+      },
+    },
+    {
+      breakpoint: 575,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+      },
+    },
+  ],
 });
 
+// переменные
 const accordionItems = Array.from(
   document.querySelectorAll(".ingredients__item")
 );
+const smoothLinks = document.querySelectorAll('a[href^="#"]');
+const timersSection = document.querySelector(".timers");
+const timers = Array.from(timersSection.querySelectorAll(".timers__count"));
 
+const timerPackage = timersSection.querySelector(".timer__count_type_package");
+const timerCountries = timersSection.querySelector(
+  ".timer__count_type_countries"
+);
+const timerCustomer = timersSection.querySelector(
+  ".timer__count_type_customer"
+);
+const timerYear = timersSection.querySelector(".timer__count_type_year");
+let startTimers = true;
+const btnUp = document.querySelector(".btn-up");
+
+const header = document.querySelector(".header");
+
+const tabs = Array.from(document.querySelectorAll(".tabs__tab"));
+const btnSearch = document.querySelector(".header__link_type_search");
+const popupSearch = document.querySelector(".popup_type_search");
+const btnVideo = document.querySelector(".video__play");
+const popupVideo = document.querySelector(".popup_type_video");
+
+const root = document.querySelector(".root");
+
+const nav = document.querySelector(".menu");
+const overlay = document.querySelector(".overlay");
+const burger = document.querySelector(".header__burger");
+const menuCloseBtn = document.querySelector(".menu__close-btn");
+const listMobileItem = Array.from(document.querySelectorAll(".menu__item"));
+
+// функции
 const hideAllAccordionItem = (items) => {
   items.forEach((item) => {
     var text = item.querySelector(".ingredients__text");
@@ -53,21 +153,6 @@ accordionItems.forEach((item) => {
   });
 });
 
-collapseAccordionItem(accordionItems[0]);
-
-const timersSection = document.querySelector(".timers");
-const timers = Array.from(timersSection.querySelectorAll(".timers__count"));
-
-const timerPackage = timersSection.querySelector(".timer__count_type_package");
-const timerCountries = timersSection.querySelector(
-  ".timer__count_type_countries"
-);
-const timerCustomer = timersSection.querySelector(
-  ".timer__count_type_customer"
-);
-const timerYear = timersSection.querySelector(".timer__count_type_year");
-let startTimers = true;
-
 const setTime = (element, value, interval, time, maxValue) => {
   let timer = setInterval(() => {
     value = value + interval;
@@ -77,14 +162,6 @@ const setTime = (element, value, interval, time, maxValue) => {
     element.textContent = value;
   }, time);
 };
-
-const btnUp = document.querySelector(".btn-up");
-
-btnUp.addEventListener("click", () => {
-  document.querySelector(".root").scrollIntoView({ behavior: "smooth" });
-});
-
-const header = document.querySelector(".header");
 
 document.addEventListener("scroll", () => {
   if (window.scrollY >= 300) {
@@ -108,8 +185,6 @@ document.addEventListener("scroll", () => {
   }
 });
 
-const tabs = Array.from(document.querySelectorAll(".tabs__tab"));
-
 const hideAllTabsItem = (items) => {
   items.forEach((item) => {
     item.classList.remove("tabs__tab_active");
@@ -131,13 +206,6 @@ tabs.forEach((tab) => {
     }
   });
 });
-
-const btnSearch = document.querySelector(".header__link_type_search");
-const popupSearch = document.querySelector(".popup_type_search");
-const btnVideo = document.querySelector(".video__play");
-const popupVideo = document.querySelector(".popup_type_video");
-
-const root = document.querySelector(".root");
 
 btnSearch.addEventListener("click", () => {
   popupSearch.classList.add("popup_show");
@@ -166,3 +234,55 @@ popupVideo.addEventListener("click", (e) => {
     root.classList.remove("root_unscroll");
   }
 });
+
+burger.addEventListener("click", () => {
+  overlay.classList.add("overlay_show");
+  nav.classList.add("menu_show");
+  root.classList.add("root_unscroll");
+  menuCloseBtn.classList.add("menu__close-btn_rotate");
+});
+
+overlay.addEventListener("click", (e) => {
+  overlay.classList.remove("overlay_show");
+  nav.classList.remove("menu_show");
+  root.classList.remove("root_unscroll");
+  menuCloseBtn.classList.remove("menu__close-btn_rotate");
+});
+
+menuCloseBtn.addEventListener("click", (e) => {
+  overlay.classList.remove("overlay_show");
+  nav.classList.remove("menu_show");
+  root.classList.remove("root_unscroll");
+  menuCloseBtn.classList.remove("menu__close-btn_rotate");
+});
+
+listMobileItem.forEach((item) => {
+  const btn = item.querySelector(".menu__drop-btn");
+  const subMenu = item.querySelector(".menu__sub-list");
+  if (btn) {
+    btn.addEventListener("click", () => {
+      subMenu.classList.toggle("menu__sub-list_show");
+      btn.classList.toggle("menu__drop-btn_rotate");
+    });
+  }
+});
+
+// стартовые функции
+if (window.scrollY >= 300) {
+  header.classList.add("header_stick");
+  btnUp.classList.add("btn-up_visible");
+}
+
+for (let smoothLink of smoothLinks) {
+  smoothLink.addEventListener("click", function (e) {
+    e.preventDefault();
+    const id = smoothLink.getAttribute("href");
+
+    document.querySelector(id).scrollIntoView({
+      behavior: "smooth",
+      block: "start",
+    });
+  });
+}
+
+collapseAccordionItem(accordionItems[0]);
